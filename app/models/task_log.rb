baac75de912f58e420a302ed624c4226f06ccf7b
Lucas Rogerio Masotti <lucasmasotti@gmail.com>
2017-07-20 21:19:09 +0000

    Fixing problem in task logs index page when a task log doesn't have a task

--- app/models/task_log.rb
+++ app/models/task_log.rb
@@ -3,4 +3,8 @@ class TaskLog < ApplicationRecord
   belongs_to :task, optional: true
   
   validates_presence_of :date
+  
+  def url
+    self.task.category_reference_url if task.present?
+  end
 end
